db4e7881db25611422918af4726cb0a6a7f99078
manny <emmanuel.medina08@gmail.com>
2019-11-11 11:48:21 -0500

    tested .get and .post with express server

--- src/server.js
+++ src/server.js
@@ -1,8 +1,16 @@
 import express from "express";
+import bodyParser from "body-parser";
 
+// Request Types: Get,Post(+requestbody),Patch,Delete
 // run: npx babel-node src/server.js
+// Added body parser dependencies
+
 const app = express();
 
+//Parses json object that is included with post request.
+app.use(bodyParser.json());
+
 app.get("/hello", (req, res) => res.send("Hello Bob!"));
+app.post("/hello", (req, res) => res.send(`Hello ${req.body.name}!`));
 
 app.listen(8000, () => console.log("Listening on port 8000"));
