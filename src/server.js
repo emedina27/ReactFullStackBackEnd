987eabcf6a5a8b2760c386f84be0616e8c670828
manny <emmanuel.medina08@gmail.com>
2019-11-11 13:32:00 -0500

    added server side  get request use express url param settings

--- src/server.js
+++ src/server.js
@@ -10,7 +10,14 @@ const app = express();
 //Parses json object that is included with post request.
 app.use(bodyParser.json());
 
-app.get("/hello", (req, res) => res.send("Hello Bob!"));
+app.get("/hello", (req, res) => res.send("Hello "));
+
+//server side
+app.get("/hello/:name"), (req, res) => res.send(`Hello ${req.params.name}`);
+
+//client side
 app.post("/hello", (req, res) => res.send(`Hello ${req.body.name}!`));
 
+
+//listen on port...
 app.listen(8000, () => console.log("Listening on port 8000"));
