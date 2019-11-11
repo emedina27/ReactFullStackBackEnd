857213e4972730dd76e494ee871d25bfb44c178f
manny <emmanuel.medina08@gmail.com>
2019-11-11 14:41:26 -0500

    completed server keepig track of upvote

--- src/server.js
+++ src/server.js
@@ -1,6 +1,18 @@
 import express from "express";
 import bodyParser from "body-parser";
 
+const articleInfo = {
+  "learn-react": {
+    upvotes: 0
+  },
+  "learn-node": {
+    upvotes: 0
+  },
+  "my-thoughts-on-resumes": {
+    upvotes: 0
+  }
+};
+
 // Request Types: Get,Post(+requestbody),Patch,Delete
 // run: npx babel-node src/server.js
 // Added body parser dependencies
@@ -10,14 +22,31 @@ const app = express();
 //Parses json object that is included with post request.
 app.use(bodyParser.json());
 
-app.get("/hello", (req, res) => res.send("Hello "));
+//define a new end point to send request to update upvotes via post request
+
+app.post("/api/articles/:name/upvote", (req, res) => {
+  //get name form params
+  const articleName = req.params.name;
+
+  //target upvotes
+  articleInfo[articleName].upvotes += 1;
+
+  //sending message
+  res
+    .status(200)
+    .send(
+      `${articleName} now has ${articleInfo[articleName].upvotes} upvotes.`
+    );
+});
+
+// app.get("/hello", (req, res) => res.send("Hello "));
 
 /* server side: server takes value out pf specified section of url and put it into       response using url params */
 
-app.get("/hello/:name", (req, res) => res.send(`Hello ${req.params.name}`));
+// app.get("/hello/:name", (req, res) => res.send(`Hello ${req.params.name}`));
 
 //client side
-app.post("/hello", (req, res) => res.send(`Hello ${req.body.name}!`));
+// app.post("/hello", (req, res) => res.send(`Hello ${req.body.name}!`));
 
 //listen on port...
 app.listen(8000, () => console.log("Listening on port 8000"));
